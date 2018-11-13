bfdf6c3836bef1797cb0f99b33c09b635a161ead
Jeff C <blackone787@gmail.com>
2018-11-12 20:52:33 -0600

    Fixing logging. Now using localStorage to validate users.

--- src/features/auth/loginService.js
+++ src/features/auth/loginService.js
@@ -1,7 +1,11 @@
 export const loginUser = (username, password) => {
   return new Promise((resolve, reject) => {
     console.log(username, password);
-    if (username === 'jcar787' && password === 'testing') {
+    const users = JSON.parse(localStorage.getItem('users'));
+    const userFound = users.find(
+      user => user.username === username && user.password === password
+    );
+    if (userFound) {
       resolve({ username });
     } else {
       reject('Bad Username or Password');
